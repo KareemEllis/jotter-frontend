93b67188668705b78fc584860debe49312a10c49
Kareem Ellis <kareemellis55@gmail.com>
2023-07-14 22:59:37 -0500

    Changed ColorPicker from using ChromePicker to TwitterPicker

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import Menu from '@mui/material/Menu'
-import { ChromePicker } from 'react-color'
+import { TwitterPicker } from 'react-color'
 
 export default function ColorPicker({ color, handleColorChange, anchorEl, setAnchorEl }) {
   const open = Boolean(anchorEl)
@@ -17,7 +17,7 @@ export default function ColorPicker({ color, handleColorChange, anchorEl, setAnc
       open={open}
       onClose={handleMenuClose}
     >
-      <ChromePicker 
+      <TwitterPicker
         color={color}
         onChange={newColor => handleColorChange(newColor.hex)}
       />
